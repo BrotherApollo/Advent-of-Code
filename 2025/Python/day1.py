6707751b1e684e25bebd6be0f3b012d012388896
BrotherApollo <cshicks96@gmail.com>
2025-12-01 17:39:37 -0500

    removing old code

--- 2025/Python/day1.py
+++ 2025/Python/day1.py
@@ -15,58 +15,6 @@ L99
 R14
 L82""".split("\n")
 
-# class Dial(): 
-    
-#     def __init__(self):
-#         self.value = 50
-#         self.zeros = 0
-        
-#     def __add__(self, value): 
-#         self.value += value
-#         while self.value > 99:
-#             self.value -= 100
-#             self.zeros += 1
-            
-#     def __sub__(self, value):
-#         self.value -= value
-#         while self.value < 0:
-#             self.value += 100
-#             self.zeros += 1
-
-# class Dial(): 
-    
-#     def __init__(self):
-#         self.value = 50
-#         self.zeros = 0
-        
-#     def turn(self, direction, value):
-#         fullturns = value // 100
-#         print(f"ripping {fullturns}")
-#         self.zeros += fullturns
-        
-#         change = value % 100
-#         print(f"changing {change}")
-        
-#         if direction == "L":
-#             self.value -= change
-#         elif direction == "R":
-#             self.value += change
-
-#         if self.value > 0 and self.value < 100:
-#             return
-        
-#         if self.value != 0:
-#             print("plus one")
-#             self.zeros += 1
-        
-#         if self.value < 0:
-#             self.value += 100
-            
-#         elif self.value > 99:
-#             self.value -= 100
-            
-#         print(self.value)
-
 class Dial(): 
     
     def __init__(self):
