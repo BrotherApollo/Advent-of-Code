64dde299900ed6d65d03eb4a7abd6f03d8e10373
BrotherApollo <cshicks96@gmail.com>
2025-12-10 17:59:42 -0500

    working on 10

---
+++ 2025/Python/day10.py
@@ -0,0 +1,64 @@
+test = "[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}"
+
+class Machine():
+    def __init__(self, rawdata):
+        self.rawdata = rawdata
+        self.formatData()
+        # Setting values for building Matrix
+        self.numLights = len(self.target)
+        self.numButtons = len(self.buttons)
+        self.min_presses = float("inf")
+        
+    def formatData(self):
+        data = self.rawdata.split(" ")
+        # Formating Target
+        self.target = [x=="#" for x in data[0].strip("[]")]
+        # Formating Joltage
+        self.joltage = data[-1].strip("{}").split(",")
+        # Formating Buttons
+        self.buttons = []
+        for button in data[1:-1]:
+            new_button = [int(x) for x in button.strip("()").split(",")]
+            self.buttons.append(new_button)
+        
+        
+
+data = test.split(" ")
+print(data)
+bot = Machine(test)
+
+lightMatrix = []
+for light_index in range(bot.numLights):
+    row = [0] * (bot.numButtons + 1)
+    for button_index, button in enumerate(bot.buttons):
+        if light_index in button:
+            row[button_index] = 1
+            
+    row[-1] = bot.target[light_index]
+    lightMatrix.append(row)
+        
+for row in lightMatrix:
+    print(row)
+print(bot.__dict__)
+
+from itertools import product
+
+for combo in product([0,1], repeat=bot.numButtons):
+    print(combo)
+    
+    lights = [0] * bot.numLights
+    presses = sum(combo)
+    
+    for index, pressed in enumerate(combo):
+        if pressed:
+            for light in bot.buttons[index]:
+                lights[light] ^= 1 #XOR opperator homie
+                
+    if lights == bot.target:
+        bot.min_presses = min(bot.min_presses, presses)
+
+print(bot.min_presses)
+
+    
+    
+
